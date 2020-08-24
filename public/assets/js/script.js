2393f8240cb1d1d723ca5be0db5524fa52d57034
Collin <collinhodgs@gmail.com>
2020-08-24 17:40:39 -0600

    fixing errors

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,7 +2,7 @@
 $(function () {
   $(".change-devoured").on("click", function (event) {
     var id = $(this).data("id");
-    var newSleep = $(this).data("devoured");
+    var newDevoured = $(this).data("devoured");
 
     var newDevouredState = {
       devoured: newDevoured,
